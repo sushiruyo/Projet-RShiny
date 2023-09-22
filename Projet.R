1385d17d1a05d6a521a524db1a98f0d907749f9d
6YanIS9 <123742387+6YanIS9@users.noreply.github.com>
2023-09-22 11:50:13 +0200

    Ajout de carte + Script Disconnect
    J'ai ajouté le script de la carte qui fonctionne et la fonction disconnect a la fin. Il faudra refaire la partie BDD et server et faire un tri dans le code.

--- Projet.R
+++ Projet.R
@@ -1,7 +1,9 @@
 library(shiny)
+library(leaflet)
 library(httr)
 library(jsonlite)
 library(RMySQL)
+library(pool)
 
 contract <- "Lyon"
 api_key <- "400699ac5a2d81c64cf5485dd8f6f20f3520e456"
@@ -23,6 +25,113 @@ con <- dbConnect(MySQL(),
                  dbname = "sql11646648")
 
 
-"Tables : communes, stations, état (api key sur état pour refresh)"
-"hello"
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("R SHINY PROJECT"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Nombre de bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      
+      
+      tabsetPanel(type = "tabs",
+                  tabPanel("Plot", plotOutput("distPlot")),
+                  tabPanel("2e", plotOutput("dist2plot"))
+      )
+    )
+  )
+)
 
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white',
+         xlab = 'Waiting time to next eruption (in mins)',
+         main = 'Histogram of waiting times')
+  })
+  
+  output$dist2plot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    pie(x, breaks = bins, col = 'red', border = 'white',
+        xlab = 'Waiting time to next eruption (in mins)',
+        main = 'Test')
+  })
+}
+
+
+
+
+
+
+# Carte
+
+# Exemple de dataframe. Remplacez ceci par votre propre dataframe.
+positions <- data.frame(
+  lat = df$position.lat,
+  long = df$position.lng
+)
+
+ui <- fluidPage(
+  leafletOutput("mymap"),
+  p(),
+  actionButton("recalc", "New points")
+)
+
+server <- function(input, output, session) {
+  
+  # Utilisez votre dataframe ici plutôt que de générer des points aléatoires
+  points <- reactive({
+    positions
+  })
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.TonerLite,
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>%
+      # Utilisez les données de votre dataframe ici
+      addMarkers(data = points())
+  })
+}
+
+shinyApp(ui, server)
+
+
+
+
+
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+# Se déconnecter de la BDD (important à utiliser car on est limités à 15 connexions)
+pool <- dbPool(
+  drv = RMySQL::MySQL(),
+  dbname = "sql11646648",
+  host = "sql11.freesqldatabase.com",
+  username = "sql11646648",
+  password = "evq3U456nu"
+)
+
+poolClose(pool)
